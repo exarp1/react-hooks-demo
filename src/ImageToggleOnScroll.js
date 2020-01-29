47e971a2115ce68c3d51a929687d60e9959aa1c1
exarp1 <exarp1@gmail.com>
2020-01-29 13:16:32 -0800

    next phase: full site with additional hooks

--- src/ImageToggleOnScroll.js
+++ src/ImageToggleOnScroll.js
@@ -1,46 +1,41 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, {useRef, useEffect, useState} from "react";
 
 const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
-  const imageRef = useRef(null);
-  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    window.addEventListener("scroll", scrollHandler);
-    setInView(isInView());
-    setIsLoading(false);
+    const imageRef = useRef(null);
+    const [isLoading,setIsLoading] = useState(true);
 
-    return () => {
-      return window.removeEventListener("scroll", scrollHandler), [isLoading];
+    useEffect(() => {
+        window.addEventListener("scroll", scrollHandler);
+        setInView(isInView());
+        setIsLoading(false);
+        return ( () => {
+            window.removeEventListener("scroll", scrollHandler);
+        });
+    },[isLoading]);
+
+    const [inView,setInView] = useState(false);
+
+    const isInView = () => {
+        if (imageRef.current) {
+            const rect = imageRef.current.getBoundingClientRect();
+            return rect.top >= 0 && rect.bottom <= window.innerHeight;
+        }
+        return false;
     };
-  });
 
-  const isInView = () => {
-    if (imageRef.current) {
-      const rect = imageRef.current.getBoundingClientRect();
-      return rect.top >= 0 && rect.bottom <= window.innerHeight;
-    }
-    return false;
-  };
+    const scrollHandler = () => {
+        setInView(() => {
+            return isInView();
+        });
+    };
 
-  const scrollHandler = () => {
-    setInView(() => {
-      return isInView();
-    });
-  };
-
-  const [inView, setInView] = useState(false);
-
-  return (
-    !isLoading && (
-      <img
-        src={inView ? secondaryImg : primaryImg}
-        alt={""}
-        ref={imageRef}
-        width="200"
-        height="200"
-      />
-    )
-  );
+    return isLoading ? null : (
+        <img
+            src={inView ? secondaryImg : primaryImg}
+            alt="" ref={imageRef} width="200" height="200"
+        />
+    );
 };
 
 export default ImageToggleOnScroll;
